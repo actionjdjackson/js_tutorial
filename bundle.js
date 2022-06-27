f3d2a82243b683aca0e7ea353ca1b3ad1414e102
Jacob Jackson <jacobjackson1985@icloud.com>
2022-06-27 15:38:16 -0500

    Now with palindrome detection

---
+++ bundle.js
@@ -0,0 +1,65 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+module.exports = Phrase;
+
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
+}
+
+String.prototype.blank = function() {
+  return !this.match(/\S/g);
+}
+
+Array.prototype.last = function() {
+  return this[this.length-1];
+}
+
+function Phrase(content) {
+  this.content = content;
+
+  this.processor = function(string) {
+    return string.toLowerCase();
+  }
+
+  this.processedContent = function processedContent() {
+    return this.processor(this.letters());
+  }
+
+  this.letters = function letters() {
+    const letterRegex = /[a-z]/i;
+    return (this.content.match(/[a-z]/gi) || []).join("");
+  }
+
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+
+  this.louder = function louder() {
+    return this.content = this.content.toUpperCase();
+  }
+};
+
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  this.processedContent = function processedContent() {
+    return this.processor(this.translation);
+  }
+
+}
+
+TranslatedPhrase.prototype = new Phrase();
+
+},{}],2:[function(require,module,exports){
+let Phrase = require("actionjdjackson-palindrome");
+
+let string = prompt("Please enter a string for palindrome testing:");
+let phrase = new Phrase(string);
+
+if( phrase.palindrome() ) {
+  alert(`"${phrase.content}" is a palindrome!`)
+} else {
+  alert(`"${phrase.content}" is not a palindrome.`)
+}
+
+},{"actionjdjackson-palindrome":1}]},{},[2]);
